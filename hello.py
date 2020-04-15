015d426246ec86c004ca447824bfd0e139e9f1fc
chandanhs19mar1999 <chandanhs.19mar1999@gmail.com>
2020-04-15 07:19:13 +0530

    let_see

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+#__name__ which links our module to flask object
+
+@app.route("/")
+def index():
+    return "Hello Chandanhs19mar1999"
+
+if __name__ == '__main__' :
+    app.run(port=5000,debug=True)
