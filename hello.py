7d58dd919c370578cd053d1bb5409eb90269db83
chandanhs19mar1999 <chandanhs.19mar1999@gmail.com>
2020-04-15 07:48:29 +0530

    dontgiveup

--- hello.py
+++ hello.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Chandanhs19mar1999"
+    return 'Hello Chandanhs19mar1999'
 
-if __name__ == '__main__' :
-    app.run(port=5000,debug=True)
+#if __name__ == '__main__' :
+    #app.run(port=5000,debug=True)
+
+#git remote add origin https://github.com/Chandanhs19mar1999/Flaskapp.git
+#git push -u origin master 
